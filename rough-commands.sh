1e5bc3b22cf7a0d9d2b0f851f53da2a1d1e58497
anandshivam44 <anand.shivam44@yahoo.com>
2026-03-17 02:11:13 +0530

    milestone 2

--- rough-commands.sh
+++ rough-commands.sh
@@ -10,4 +10,4 @@ cd /Users/shivam.anand/personal/argocd-research && \
 
 kubectl delete applications --all -n argocd && kubectl delete applicationsets --all -n argocd
 cd /Users/shivam.anand/personal/argocd-research && \ 
-kubectl apply -f clusters/kind-shivam-playgroung-1/argocd-app-of-apps.yaml
+kubectl apply -R -f clusters/kind-shivam-playgroung-1 --context kind-shivam-playgroung-1
